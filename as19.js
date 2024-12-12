b5ac222aefd0b373300ea801a733f44f248bc2b8
Pallavi-270 <veeramanikantapallavi@gmail.com>
2024-12-12 13:30:13 +0530

    globalfunction

---
+++ as19.js
@@ -0,0 +1,14 @@
+// declare a variable
+let age=25;
+// create a function
+function displayAge(){
+    console.log("age is:",age);
+}
+// create a2nd function
+function changeAge(){
+    age=30;
+    console.log("age has been updated",age);
+}
+displayAge();
+changeAge();
+displayAge();
